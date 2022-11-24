d31a8d8de3593f0541b2d9e18eb434888d457e92
Leonardo Carvalho <leonardoscc15@gmail.com>
2022-11-23 22:44:04 -0300

    Add three.js camera and orbit controller

--- src/main.js
+++ src/main.js
@@ -5,3 +5,13 @@ document.querySelector(".webGLContainer").appendChild(renderer.domElement);
 
 // SCENE SETUP
 const scene = new THREE.Scene();
+
+// CAMERA AND ORBIT CONTROLLER
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+camera.position.set(0, 5, 10);
+camera.lookAt(0, 0, 0);
