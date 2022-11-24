18023c3ca8fbb6925754480f57183c7e1efc25ad
Leonardo Carvalho <leonardoscc15@gmail.com>
2022-11-23 22:43:34 -0300

    Add three.js scene setup

--- src/main.js
+++ src/main.js
@@ -2,3 +2,6 @@
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.querySelector(".webGLContainer").appendChild(renderer.domElement);
+
+// SCENE SETUP
+const scene = new THREE.Scene();
