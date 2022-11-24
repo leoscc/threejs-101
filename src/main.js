58d16aab11a3e0b2ad10fb711c0503363c5117ec
Leonardo Carvalho <leonardoscc15@gmail.com>
2022-11-23 22:43:09 -0300

    Add three.js renderer setup

---
+++ src/main.js
@@ -0,0 +1,4 @@
+// RENDERER SETUP
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.querySelector(".webGLContainer").appendChild(renderer.domElement);
